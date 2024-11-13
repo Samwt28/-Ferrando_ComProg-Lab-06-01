bef9ea350a1cabf9342d818d86a33b8c7f2dc347
Samwt28 <canttouchthisx.onlineclass@gmail.com>
2024-11-13 10:04:44 +0800

    Update JavaApplication3.java

--- JavaApplication3.java
+++ JavaApplication3.java
@@ -1,28 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication3;
 
+package javaapplication3;
 
 public class JavaApplication3 {
 
-    
     public static void main(String[] args) {
         int iNumber = 3;
-        
-        if (iNumber >= 0)
-           
-        if (iNumber == 0)
-            System.out.println("first string");
-            
-        else
-            System.out.println("second string");
-            System.out.println("third string");
-        
-        
-         
+
+        if (iNumber >= 0) {
+            if (iNumber == 0) {
+                System.out.println("first string");
+            } else {
+                System.out.println("second string");
+                System.out.println("third string");
+            }
+        }
     }
-    
+
 }
